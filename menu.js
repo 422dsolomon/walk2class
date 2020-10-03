b077745257563db51a83263494c728bda7259190
Aidan Keenan <aidankeenan@Aidans-MacBook-Pro.local>
2020-10-03 17:19:19 -0400

    add map

--- menu.js
+++ menu.js
@@ -5,3 +5,14 @@
       $(".menu-container").load("menu.html");
     });
   })();
+
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 42.22283, lng: 71.07015 },
+    zoom: 8,
+  });
+}
+
